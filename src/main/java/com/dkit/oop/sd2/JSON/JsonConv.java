9f0e44246e55c581240bdb3c4b585926bcb217e6
Meghank1066 <D00254682@student.dkit.ie>
2024-04-05 23:57:37 +0100

    json pom file updated with gson dependancy to allow for json converstion to a string :)

---
+++ src/main/java/com/dkit/oop/sd2/JSON/JsonConv.java
@@ -0,0 +1,13 @@
+//package com.dkit.oop.sd2.JSON;
+//
+//
+//import com.google.gson.Gson;
+//import com.dkit.oop.sd2.DTOs.Task;
+//
+//public class JsonConv {
+//
+//    private Gson gsonParser;
+//
+//
+//
+//}
